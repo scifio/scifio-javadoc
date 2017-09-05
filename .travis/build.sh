eae1a89575771b5cb133c73330f61aca9d73f1ff
Curtis Rueden <ctrueden@wisc.edu>
2017-09-05 09:33:24 -0500

    Update Travis's SSH key

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-javadoc.sh
-sh travis-javadoc.sh SCIFIO
+sh travis-javadoc.sh SCIFIO $encrypted_3abd943e8c6d_key $encrypted_3abd943e8c6d_iv
